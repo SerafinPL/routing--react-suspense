307d0a36c98523d3d8906e23fbed26b50ff497b0
serafinpl <ser.burberibar@gmail.com>
2021-02-28 21:03:25 +0100

    improve the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
-import Posts from './containers/Posts';
+
 import User from './containers/User';
 import Welcome from './containers/Welcome';
+const Posts = React.lazy(() => import('./containers/Posts'));
+const Contact = React.lazy(() => import('./containers/Contact'));
 
 class App extends Component {
+
+  state = {
+    showContact: false
+  }
+
+  toggleContact = () => {
+    this.setState(prevState => {
+      return {showContact: !prevState.showContact};
+    });
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          <Route path="/posts" component={Posts} />
-        </React.Fragment>
-      </BrowserRouter>
+      <React.Fragment>
+        <BrowserRouter>
+          <React.Fragment>
+            <nav>
+              <NavLink to="/user">User Page</NavLink> |&nbsp;
+              <NavLink to="/posts">Posts Page</NavLink>
+            </nav>
+            <Route path="/" component={Welcome} exact />
+            <Route path="/user" component={User} />
+            <Route 
+              path="/posts" 
+              render={() => (
+                <Suspense fallback={<div>...Loading</div>}>
+                  <Posts />
+                </Suspense>
+                )} 
+            />
+          </React.Fragment>
+        </BrowserRouter>
+        <button onClick={this.toggleContact}>Toggle Contact</button>
+        {this.state.showContact ? (
+          <Suspense fallback={<div>...Loading</div>}>
+            <Contact />
+          </Suspense>
+          ) : null}
+
+      </React.Fragment>   
     );
   }
 }
